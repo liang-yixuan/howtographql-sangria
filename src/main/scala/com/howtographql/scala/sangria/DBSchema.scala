2f21d2f52020470d9f3e64ff92f7ee08ad11624e
Yixuan Liang <yixuan.liang@rea-group.com>
2022-06-28 22:13:40 +1000

    The first query
    0. Model data object and context object in package.scala
    1. Set up DB Schema
    2. Set up GraphQL Schema in GraphQL server
    3. Define resolver function in DAO (Data Access Object)
    4. Route to GraphQL server in the main server

--- src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
+++ src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
@@ -5,17 +5,37 @@ import slick.jdbc.H2Profile.api._
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.language.postfixOps
-
+import com.howtographql.scala.sangria.models._
 
 object DBSchema {
 
-  /**
-    * Load schema and populate sample data withing this Sequence od DBActions
-    */
+  // 1 defines mapping to database table,
+  class LinksTable(tag: Tag) extends Table[Link](tag, "LINKS") {
+
+    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+    def url = column[String]("URL")
+    def description = column[String]("DESCRIPTION")
+
+    def * = (id, url, description).mapTo[Link]
+
+  }
+
+  // 2 gives us a helper we will use to accessing data in this table.
+  val Links = TableQuery[LinksTable]
+
+  // 3 is responsible for creating schema and adding three entities to the database.
   val databaseSetup = DBIO.seq(
-
+    Links.schema.create,
+    Links forceInsertAll Seq(
+      Link(
+        1,
+        "http://howtographql.com",
+        "Awesome community driven GraphQL tutorial"
+      ),
+      Link(2, "http://graphql.org", "Official GraphQL web page"),
+      Link(3, "https://graphql.org/", "GraphQL specification")
+    )
   )
-
 
   def createDatabase: DAO = {
     val db = Database.forConfig("h2mem")
