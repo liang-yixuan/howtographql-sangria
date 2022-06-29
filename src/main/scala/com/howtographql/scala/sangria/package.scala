ada11a64f59ddf50b86670f7a1c98b76a345f368
Yixuan Liang <yixuanliang97@gmail.com>
2022-06-29 23:09:41 +1000

    minor fix for MyContext class

--- src/main/scala/com/howtographql/scala/sangria/package.scala
+++ src/main/scala/com/howtographql/scala/sangria/package.scala
@@ -2,5 +2,5 @@ package com.howtographql.scala.sangria
 
 package object models {
   case class Link(id: Int, url: String, description: String)
-  case class MyContext(dao: DAO) {}
+  case class MyContext(dao: DAO)
 }
